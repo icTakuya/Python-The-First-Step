871f5f2133f1c00d6ac75c607812e07c13b419a5
Takuya <tlog.since2019@gmail.com>
2022-01-30 21:06:59 +0900

    'Add lesson14'

--- Python_3_Introduction/lesson14.py
+++ Python_3_Introduction/lesson14.py
@@ -5,7 +5,37 @@ class Person(object):
     def __init__(self):
         self.x = 100
 
-a = Person()
-print(a.x)
+    def what_is_your_kind(self):
+        return self.kind
+    
+    # 下記のようにclassメソッドとして表記するとオブジェクト生成しなくとも使用可能
+    # @classmethod
+    # def what_is_your_kind(cls):
+    #     return cls.kind
 
-b = 
+    @staticmethod
+    def about(year):
+        print('about human {}'.format(year))
+
+
+a = Person()
+print(a) #オブジェクトですよと返ってくる
+print(a.x) # 100
+print(a.kind)
+print(a.what_is_your_kind())
+
+
+print('#########')
+b = Person #オブジェクト生成しない
+print(b)
+# print(b.x) オブジェクトではないのでアクセスできない
+print(b.kind) #クラス変数はアクセスできる
+# print(b.what_is_your_kind()) オブジェクト化してないのでselfが自分自身でない
+
+# クラスメソッドの使用
+# print('#########')
+# print(Person.kind)
+# print(Person.what_is_your_kind())
+
+# スタティックメソッドの使用
+Person.about(1999)
